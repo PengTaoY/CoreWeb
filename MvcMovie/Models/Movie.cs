cd0d2e30d7ff95830bee62516752c4615d8cdedf
PengtaoY <1197726544@QQ.com>
2017-04-27 21:57:51 +0800

    添加Model.但是EF的Model生成数据库的时候出错了。不得不自己在视图-》SqlServer对象资源管理器中自己手动根据Appsetting.json中的数据库名手动创建的数据库跟数据表

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
