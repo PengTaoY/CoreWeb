bd871d809fcf01dc4687ddc448b1e91e6a70b8d7
PengtaoY <1197726544@QQ.com>
2017-04-30 11:36:39 +0800

    重试EFCore。可以添加成功。之前的错误，没找到是哪一步出错的

---
+++ TestEFCore/Data/TestEFCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestEFCore.Models
+{
+    public class TestEFCoreContext : DbContext
+    {
+        public TestEFCoreContext (DbContextOptions<TestEFCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestEFCore.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ TestEFCore/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TestEFCore.Models;
+
+namespace TestEFCore
+{
+    public class Startup
+    {
+        #region 1.1 Add a constructor
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        #endregion
+
+        #region 1.2 Add MVC services:
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddDbContext<TestEFCoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TestEFCoreContext")));
+        }
+        #endregion
+
+
+        #region 1.3 Configure web app to use Configuration and use MVC routing:
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        } 
+        #endregion
+    }
+}
